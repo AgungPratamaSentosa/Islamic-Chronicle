cfb5c6aee844260414d59768cebd4a6c9b044835
agung pratama <agung.agungps2005@gmail.com>
2024-11-14 18:05:17 +0700

    commit

---
+++ app/src/main/java/com/ebookonline/MainActivity.java
@@ -0,0 +1,44 @@
+package com.ebookonline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final LinearLayout bukuelektronBtn = findViewById(R.id.bukuelektronBtn);
+        final LinearLayout komponenBtn = findViewById(R.id.komponenBtn);
+        final LinearLayout dwBtn = findViewById(R.id.dwBtn);
+
+        bukuelektronBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, webview.class);
+                startActivity(intent);
+            }
+        });
+
+        komponenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, web.class);
+                startActivity(intent);
+            }
+        });
+        dwBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, web.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
